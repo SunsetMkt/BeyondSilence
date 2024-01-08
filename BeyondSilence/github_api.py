48e2484bc4471c833707c960b05bcdebfe5b6abf
Sunset Mikoto <26019675+lwd-temp@users.noreply.github.com>
2024-01-08 15:11:41 +0800

    Wait time logging

--- BeyondSilence/github_api.py
+++ BeyondSilence/github_api.py
@@ -22,7 +22,8 @@ def get_recent_activities_of(user_name):
             reset_time = int(req.headers.get("x-ratelimit-reset"))
             logging.info(f"Rate limit reset time: {reset_time}")
             wait_time = reset_time - time.time() + 1
-            if wait_time < 0: # Failsafe
+            logging.info(f"Wait time: {wait_time}")
+            if wait_time < 0:  # Failsafe
                 wait_time = 0
             req = None  # Reset req
             time.sleep(wait_time)
